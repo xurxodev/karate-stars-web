8c95bd805f951e540cfe15cb0a52715e10f21881
Jorge Sánchez <xurxosf@gmail.com>
2019-12-30 06:56:16 +0100

    Rename to CurrentNewsRSSRepository

--- src/data/currentnews/CurrentNewsRSSRepository.ts
+++ src/data/currentnews/CurrentNewsRSSRepository.ts
@@ -3,7 +3,7 @@ import Parser from "rss-parser";
 import CurrentNewsRepository from "../../domain/currentnews/boundaries/CurrentNewsRepository";
 import { CurrentNews, NewsSource } from "../../domain/currentnews/entities/CurrentNews";
 
-export default class CurrentNewsNetworkRepository implements CurrentNewsRepository {
+export default class CurrentNewsRSSRepository implements CurrentNewsRepository {
 
   public parser = new Parser();
 
