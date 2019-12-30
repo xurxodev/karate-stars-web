85485b6fc04fdafb1acb7cfcff7d5ab9faae5ef3
Jorge Sánchez <xurxosf@gmail.com>
2019-12-30 06:47:23 +0100

    Fix CurrentNewsRSSRepository file name

--- src/api/currentnews/CurrentNewsRoutes.ts
+++ src/api/currentnews/CurrentNewsRoutes.ts
@@ -1,12 +1,12 @@
 import * as hapi from "hapi";
 
-import CurrentNewsRSSRepository from "../../data/currentnews/CurrentNewsRSSRepository";
+import CurrentNewsRepository from "../../data/currentnews/CurrentNewsRSSRepository";
 import GetCurrentNewsUseCase from "../../domain/currentnews/usecases/GetCurrentNewsUseCase";
 import jwtAuthentication from "../users/JwtAuthentication";
 import CurrentNewsController from "./CurrentNewsController";
 
 export default function(): hapi.ServerRoute[] {
-  const currentNewsRepository = new CurrentNewsRSSRepository();
+  const currentNewsRepository = new CurrentNewsRepository();
   const getCurrentNewsUseCase = new GetCurrentNewsUseCase(currentNewsRepository);
   const currentNewsController = new CurrentNewsController(getCurrentNewsUseCase);
 
