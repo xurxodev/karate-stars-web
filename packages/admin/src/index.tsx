1fba31fa8c09ff966773eaef1e32baa008445655
Jorge Sánchez <xurxosf@gmail.com>
2020-05-02 13:41:08 +0200

    Create app folder

--- packages/admin/src/index.tsx
+++ packages/admin/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
-import App from "./App";
+import App from "./app/App";
 import theme from "./theme";
 
 ReactDOM.render(
