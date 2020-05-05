858c0d335214edcb3ca700bf722d0db21c0ce507
Jorge Sánchez <xurxosf@gmail.com>
2020-05-05 18:20:26 +0200

    Use BrowserRouter

--- packages/admin/src/app/App.tsx
+++ packages/admin/src/app/App.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
 import "./App.css";
 import { ThemeProvider, CssBaseline } from "@material-ui/core";
 import theme from "./theme";
 
 const App: React.FC = () => {
-    const browserHistory = createBrowserHistory();
-
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Router history={browserHistory}>
+            <BrowserRouter>
                 <Routes />
-            </Router>
+            </BrowserRouter>
         </ThemeProvider>
     );
 };
