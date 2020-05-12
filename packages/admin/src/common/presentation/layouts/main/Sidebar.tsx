29b5d8a422bb260dd5b7355a4cbbbaeb1ae99709
Jorge Sánchez <xurxosf@gmail.com>
2020-05-12 18:04:59 +0200

    Add logout button in drawer for mobile screens

--- packages/admin/src/common/presentation/layouts/main/Sidebar.tsx
+++ packages/admin/src/common/presentation/layouts/main/Sidebar.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import { Divider, Drawer, Theme } from "@material-ui/core";
+import { Divider, Drawer, Theme, Button, colors, Hidden } from "@material-ui/core";
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import PeopleIcon from "@material-ui/icons/People";
 import SettingsIcon from "@material-ui/icons/Settings";
 import Notifications from "@material-ui/icons/Notifications";
 import VideoLibrary from "@material-ui/icons/VideoLibrary";
+import InputIcon from "@material-ui/icons/Input";
 
 import SidebarNav from "./SidebarNav";
 import Profile from "./Profile";
 import { pages } from "../../../../app/AppRoutes";
+import { useAppBlocContext } from "../../../../app/AppContext";
 
 const useStyles = makeStyles((theme: Theme) => ({
     drawer: {
@@ -32,6 +34,26 @@ const useStyles = makeStyles((theme: Theme) => ({
     nav: {
         marginBottom: theme.spacing(2),
     },
+    logoutButton: {
+        color: colors.blueGrey[800],
+        padding: "10px 8px",
+        justifyContent: "flex-start",
+        textTransform: "none",
+        letterSpacing: 0,
+        width: "100%",
+        fontWeight: theme.typography.fontWeightMedium,
+    },
+    logoutIcon: {
+        // color: theme.palette.icon,
+        width: 24,
+        height: 24,
+        display: "flex",
+        alignItems: "center",
+        marginRight: theme.spacing(1),
+    },
+    flexGrow: {
+        flexGrow: 1,
+    },
 }));
 
 interface SidebarProps {
@@ -42,6 +64,9 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ open, variant, onClose }) => {
     const classes = useStyles();
+    const appBloc = useAppBlocContext();
+
+    const handleLogout = () => appBloc.logout();
 
     const menus = [
         {
@@ -82,6 +107,17 @@ const Sidebar: React.FC<SidebarProps> = ({ open, variant, onClose }) => {
                 <Profile />
                 <Divider className={classes.divider} />
                 <SidebarNav className={classes.nav} menus={menus} />
+
+                <div className={classes.flexGrow} />
+
+                <Hidden lgUp>
+                    <Button className={classes.logoutButton} onClick={handleLogout}>
+                        <div className={classes.logoutIcon}>
+                            <InputIcon />
+                        </div>
+                        {"Logout"}
+                    </Button>
+                </Hidden>
             </div>
         </Drawer>
     );
