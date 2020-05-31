f41d7aa348f370ad560031d4424489330f861291
Jorge Sánchez <xurxosf@gmail.com>
2020-05-31 13:49:41 +0200

    Fix test template

--- packages/admin/src/app/__test__/App.spec.tsx
+++ packages/admin/src/app/__test__/App.spec.tsx
@@ -1,7 +1,8 @@
+import { pages } from "../AppRoutes";
+
 test("renders learn react link", () => {
+    expect(pages).toBeTruthy();
     // const { getByText } = render(<App />);
     // const linkElement = getByText(/learn react/i);
     // expect(linkElement).toBeInTheDocument();
 });
-
-export {};
