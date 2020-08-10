d858aaf2d20025e268e578a24b5952f8bc68bfde
Jorge Sánchez <xurxosf@gmail.com>
2020-08-10 18:53:19 +0200

    Add login ok and login error tests

--- packages/admin/cypress/integration/login.spec.ts
+++ packages/admin/cypress/integration/login.spec.ts
@@ -1,7 +1,31 @@
+const username = Cypress.env("USERNAME");
+const password = Cypress.env("PASSWORD");
+
+//TODO: Try Cypress testing library
+// With Cypress testing library
+// cy.get("Email]").type(username);
+// cy.get("Password]").type(password);
+// cy.get("SIGN IN NOW").click();
+
 context("Login page", () => {
-    it("has page title", () => {
+    beforeEach(() => {
         cy.visit("/");
-        cy.contains("Login")
-        cy.url().should('include', '/login')
+        cy.contains("Login");
+        cy.url().should("include", "/login");
+    })
+
+    it("should realize login", () => {
+        cy.get('input[name=email]').type(username);
+        // {enter} causes the form to submit
+        cy.get('input[name=password]').type(`${password}{enter}`);
+
+        cy.url().should('include', '/dashboard');
+    });
+    it("should show invalid credentials if password is wrong", () => {
+        cy.get('input[name=email]').type(username);
+        // {enter} causes the form to submit
+        cy.get('input[name=password]').type(`Wrong password{enter}`)
+
+        cy.contains("Invalid credentials")
     });
 });
