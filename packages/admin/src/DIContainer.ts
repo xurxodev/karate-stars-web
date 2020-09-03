5201a42b050666d82da0c2ed47994010b7833a80
Jorge Sánchez <xurxosf@gmail.com>
2020-09-03 15:30:27 +0200

    Create DIContainer

---
+++ packages/admin/src/DIContainer.ts
@@ -0,0 +1,58 @@
+interface Type<T> {
+    new (...args: any[]): T;
+}
+
+type Token<T> = Type<T> | string;
+
+type BinderType = "lazySingleton" | "factory";
+
+type Binder<T> = {
+    type: BinderType;
+    fn: () => T;
+};
+
+export default class DIContainer {
+    private factories = new Map<Token<any>, Binder<any>>();
+    private lazySingletons = new Map<Token<any>, any>();
+
+    private static instance: DIContainer;
+    private constructor() {}
+
+    static getInstance(): DIContainer {
+        if (!DIContainer.instance) {
+            DIContainer.instance = new DIContainer();
+        }
+
+        return DIContainer.instance;
+    }
+
+    public get<T>(token: Type<T> | string): T {
+        const factory = this.factories.get(token);
+
+        if (!factory) {
+            throw new Error(`Dependency ${token} is not regestered`);
+        }
+
+        if (factory.type === "lazySingleton") {
+            const singleton = this.lazySingletons.get(token) || factory.fn();
+            this.lazySingletons.set(token, singleton);
+
+            return singleton;
+        } else {
+            return factory.fn();
+        }
+    }
+
+    public bindFactory<T>(token: Type<T> | string, fn: () => T) {
+        this.factories.set(token, { type: "factory", fn });
+    }
+
+    public bindLazySingleton<T>(token: Type<T> | string, fn: () => T) {
+        this.factories.set(token, { type: "lazySingleton", fn });
+    }
+
+    public clear() {
+        this.factories.clear();
+        this.lazySingletons.clear();
+    }
+}
