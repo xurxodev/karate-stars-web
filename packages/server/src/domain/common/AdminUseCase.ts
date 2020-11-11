695ca80f7a293f6fec9284200fc874718117ebec
Jorge Sánchez <xurxosf@gmail.com>
2020-11-11 14:21:50 +0100

    Refactors in AdmidUseCase to can assign custom errors

--- packages/server/src/domain/common/AdminUseCase.ts
+++ packages/server/src/domain/common/AdminUseCase.ts
@@ -3,18 +3,18 @@ import { PermissionError } from "../../api/authentication/PermisionError";
 import { ResourceNotFound, UnexpectedError } from "../../api/common/Errors";
 import UserRepository from "../users/boundaries/UserRepository";
 
-export interface WithUserIdArgs {
+export interface AdminUseCaseArgs {
     userId: string;
 }
 
 export type AdminUseCaseError = ResourceNotFound | PermissionError | UnexpectedError;
 
-export abstract class AdminUseCase<Arguments extends WithUserIdArgs, Data> {
+export abstract class AdminUseCase<Arguments extends AdminUseCaseArgs, Error, Data> {
     constructor(private userRepository: UserRepository) {}
 
-    public abstract async run(arg: Arguments): Promise<Either<AdminUseCaseError, Data>>;
+    public abstract async run(arg: Arguments): Promise<Either<Error, Data>>;
 
-    public async execute(arg: Arguments): Promise<Either<AdminUseCaseError, Data>> {
+    public async execute(arg: Arguments): Promise<Either<AdminUseCaseError | Error, Data>> {
         try {
             const userError = await this.validateUserId(arg.userId);
 
@@ -34,35 +34,30 @@ export abstract class AdminUseCase<Arguments extends WithUserIdArgs, Data> {
     private async validateUserId(userId: string): Promise<AdminUseCaseError | null> {
         const idResult = Id.createExisted(userId);
 
-        // try with fold
-
-        if (idResult.isLeft()) {
-            return {
+        return await idResult.fold<Promise<AdminUseCaseError | null>>(
+            async () => ({
                 kind: "ResourceNotFound",
                 message: `User with id ${userId} not found`,
-            };
-        } else {
-            return await this.validateIdUserIsAdmin(idResult.get());
-        }
+            }),
+            async () => this.validateIdUserIsAdmin(idResult.get())
+        );
     }
 
     private async validateIdUserIsAdmin(userId: Id): Promise<AdminUseCaseError | null> {
         const userResult = await this.userRepository.getByUserId(userId);
 
-        // try with fold
-
-        if (!userResult.isDefined()) {
-            return {
+        return userResult.fold<AdminUseCaseError | null>(
+            () => ({
                 kind: "ResourceNotFound",
                 message: `User with id ${userId.value} not found`,
-            };
-        } else if (!userResult.get().isAdmin) {
-            return {
-                kind: "PermissionError",
-                message: `You have not permissions to access to this resource. Only admin users can accesss to this resource`,
-            };
-        } else {
-            return null;
-        }
+            }),
+            user =>
+                user.isAdmin
+                    ? null
+                    : {
+                          kind: "PermissionError",
+                          message: `You have not permissions to access to this resource. Only admin users can accesss to this resource`,
+                      }
+        );
     }
 }
--- packages/server/src/domain/newsFeeds/usecases/GetNewsFeedsUseCase.ts
+++ packages/server/src/domain/newsFeeds/usecases/GetNewsFeedsUseCase.ts
@@ -1,14 +1,19 @@
 import { Either, NewsFeedRawData } from "karate-stars-core";
-import { AdminUseCase, AdminUseCaseError, WithUserIdArgs } from "../../common/AdminUseCase";
+import { UnexpectedError } from "../../../api/common/Errors";
+import { AdminUseCase, AdminUseCaseArgs } from "../../common/AdminUseCase";
 import UserRepository from "../../users/boundaries/UserRepository";
 import NewsFeedsRepository from "../boundaries/NewsFeedRepository";
 
-export class GetNewsFeedsUseCase extends AdminUseCase<WithUserIdArgs, NewsFeedRawData[]> {
+export class GetNewsFeedsUseCase extends AdminUseCase<
+    AdminUseCaseArgs,
+    UnexpectedError,
+    NewsFeedRawData[]
+> {
     constructor(private newsFeedsRepository: NewsFeedsRepository, userRepository: UserRepository) {
         super(userRepository);
     }
 
-    public async run(_: WithUserIdArgs): Promise<Either<AdminUseCaseError, NewsFeedRawData[]>> {
+    public async run(_: AdminUseCaseArgs): Promise<Either<UnexpectedError, NewsFeedRawData[]>> {
         const newsFeed = await this.newsFeedsRepository.getAll();
 
         return Either.right(newsFeed.map(newsFeed => newsFeed.toRawData()));
