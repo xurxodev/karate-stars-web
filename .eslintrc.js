ad02e57f56a90bff8972ffb53f3e1c24525d2f03
Jorge Sánchez <xurxosf@gmail.com>
2021-02-11 16:32:02 +0100

    Split eslintrc.js for root and react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     extends: [
-        "react-app",
-        "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:prettier/recommended",
         "prettier/@typescript-eslint",
     ],
     parserOptions: {
-        ecmaVersion: 2018,
+        ecmaVersion: 2020,
         sourceType: "module",
-        ecmaFeatures: {
-            jsx: true,
-        },
     },
     rules: {
         "no-console": "off",
         "@typescript-eslint/explicit-function-return-type": ["off"],
         "@typescript-eslint/explicit-module-boundary-types": ["off"],
         "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-        "react/prop-types": "off",
         "no-unused-expressions": "off",
         "no-useless-concat": "off",
         "no-useless-constructor": "off",
@@ -29,11 +23,5 @@ module.exports = {
         "@typescript-eslint/no-empty-interface": "off",
         "@typescript-eslint/ban-ts-ignore": "off",
         "@typescript-eslint/no-empty-function": "off",
-        "react-hooks/exhaustive-deps": "off",
-    },
-    settings: {
-        react: {
-            version: "detect",
-        },
     },
 };
---
+++ packages/admin/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: ["../../.eslintrc.js", "react-app", "plugin:react/recommended"],
+    parser: "@typescript-eslint/parser",
+    env: {
+        node: true,
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        "react/prop-types": "off",
+        "react-hooks/exhaustive-deps": "off",
+        //"react/jsx-uses-react": "off",
+        //"react/react-in-jsx-scope": "off",
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+};
