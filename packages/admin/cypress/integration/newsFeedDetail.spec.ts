ebefd54f1ce5c6209440fca8f1201c3251dfbf06
Jorge Sánchez <xurxosf@gmail.com>
2021-06-22 18:03:15 +0200

    Fix cypress tests

--- packages/admin/cypress/integration/newsFeedDetail.spec.ts
+++ packages/admin/cypress/integration/newsFeedDetail.spec.ts
@@ -1,17 +1,17 @@
 describe("News Feeds page", () => {
     describe("New", () => {
         beforeEach(() => {
-            cy.login()
+            cy.login();
             cy.visit("#/news-feeds/new");
 
-            cy.intercept('POST', '/api/v1/news-feeds', {
+            cy.intercept("POST", "/api/v1/news-feeds", {
                 statusCode: 201,
                 body: {
-                    "ok": true,
-                    "count": 1
-                }
+                    ok: true,
+                    count: 1,
+                },
             });
-        })
+        });
 
         it("should create a new item", () => {
             cy.findByLabelText("Name (*)").type("Xurxo dev");
@@ -19,7 +19,7 @@ describe("News Feeds page", () => {
             cy.findByLabelText("Language (*)").type("es");
             cy.findByLabelText("Type (*)").select("RSS");
 
-            cy.findByText('Accept').click();
+            cy.findByText("Accept").click();
 
             cy.findByText("News feed saved!");
         });
@@ -27,24 +27,24 @@ describe("News Feeds page", () => {
 
     describe("Edit", () => {
         beforeEach(() => {
-            cy.login()
-            cy.visit("#/news-feeds/edit/fvTIvWAVhyn");
+            cy.login();
+            cy.visit("#/news-feeds/edit/llMKGYg0Ri1");
 
-            cy.intercept('PUT', '/api/v1/news-feeds', {
+            cy.intercept("PUT", "/api/v1/news-feeds", {
                 statusCode: 200,
                 body: {
-                    "ok": true,
-                    "count": 1
-                }
+                    ok: true,
+                    count: 1,
+                },
             });
-        })
+        });
         it("should edit an item", () => {
             cy.findByLabelText("Name (*)").type("Xurxo dev");
             cy.findByLabelText("Url (*)").type("https://xurxodev.com/");
             cy.findByLabelText("Language (*)").type("es");
             cy.findByLabelText("Type (*)").select("RSS");
 
-            cy.findByText('Accept').click();
+            cy.findByText("Accept").click();
 
             cy.findByText("News feed saved!");
         });
