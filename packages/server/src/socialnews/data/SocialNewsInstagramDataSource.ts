51685e06a898d55bbbc2b45dade670294a30047e
Jorge Sánchez <xurxosf@gmail.com>
2021-12-05 18:32:05 +0100

    try instatouch

--- packages/server/src/socialnews/data/SocialNewsInstagramDataSource.ts
+++ packages/server/src/socialnews/data/SocialNewsInstagramDataSource.ts
@@ -1,6 +1,9 @@
 import SocialNewsRepository from "../domain/boundaries/SocialNewsRepository";
 import { SocialNews, SocialUser } from "../domain/entities/SocialNews";
 import fetch from "node-fetch";
+import { user, getUserMeta } from "instatouch";
+import { Post, PostCollector } from "instatouch/build/types";
+//const instaTouch = require("instatouch");
 
 export default class SocialNewsInstagramDataSource implements SocialNewsRepository {
     constructor() {}
@@ -18,35 +21,60 @@ export default class SocialNewsInstagramDataSource implements SocialNewsReposito
     }
 
     public async getSocialNewsBy(username: string): Promise<SocialNews[]> {
-        const profile = await this.get_instagram_profile(username);
+        // const profile = await this.get_instagram_profile(username);
 
-        const socialUser = {
-            name: profile.graphql.user.full_name,
-            userName: profile.graphql.user.username,
-            image: profile.graphql.user.profile_pic_url,
-            url: `https://www.instagram.com/${profile.graphql.user.username}`,
-        };
+        // const socialUser = {
+        //     name: profile.graphql.user.full_name,
+        //     userName: profile.graphql.user.username,
+        //     image: profile.graphql.user.profile_pic_url,
+        //     url: `https://www.instagram.com/${profile.graphql.user.username}`,
+        // };
 
-        const socialNews = profile.graphql.user.edge_owner_to_timeline_media.edges.map(
-            (media: any) => this.mapMedia(socialUser, media)
-        );
+        // const socialNews = profile.graphql.user.edge_owner_to_timeline_media.edges.map(
+        //     (media: any) => this.mapMedia(socialUser, media)
+        // );
 
-        return socialNews;
+        try {
+            const options = { count: 10, session: "sessionid=4565290142%3AiNbfZeKnoYDuhF%3A17" };
+
+            const userMeta = await getUserMeta(username, options);
+            const socialUser = {
+                name: userMeta.graphql.user.full_name,
+                userName: userMeta.graphql.user.username,
+                image: userMeta.graphql.user.profile_pic_url,
+                url: `https://www.instagram.com/${userMeta.graphql.user.username}`,
+            };
+
+            const posts = await user(username, options);
+            console.log({ posts });
+
+            const socialNews = posts.collector.map((media: PostCollector) =>
+                this.mapMedia(socialUser, media)
+            );
+
+            console.log({ socialNews });
+            return socialNews;
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
     }
 
-    private mapMedia(socialUser: SocialUser, media: any): SocialNews {
-        const date = new Date(new Date(media.node.taken_at_timestamp * 1000)).toISOString();
+    private mapMedia(socialUser: SocialUser, media: PostCollector): SocialNews {
+        const date = new Date(
+            media.taken_at_timestamp ? new Date(media.taken_at_timestamp * 1000) : new Date()
+        ).toISOString();
 
-        const image = !media.node.is_video ? media.node.display_url : undefined;
+        const image = !media.is_video ? media.display_url : undefined;
 
-        const video = media.node.is_video ? media.node.video_url : undefined;
+        const video = media.is_video ? media.video_url : undefined;
 
-        const url = `https://www.instagram.com/p/${media.node.shortcode}`;
+        const url = `https://www.instagram.com/p/${media.shortcode}`;
 
         return {
             network: "instagram",
             summary: {
-                title: media.node.edge_media_to_caption.edges[0].node.text,
+                title: media.text ?? "",
                 image,
                 video,
                 date,
@@ -56,16 +84,16 @@ export default class SocialNewsInstagramDataSource implements SocialNewsReposito
         };
     }
 
-    async get_instagram_profile(username) {
-        const response = await fetch(`https://www.instagram.com/${username}/channel/?__a=1`, {
-            headers: {
-                "User-Agent":
-                    "Instagram 85.0.0.21.100 Android (23/6.0.1; 538dpi; 1440x2560; LGE; LG-E425f; vee3e; en_US)",
-            },
-        }).then(response => response.json());
+    // async get_instagram_profile(username) {
+    //     const response = await fetch(`https://www.instagram.com/${username}/channel/?__a=1`, {
+    //         headers: {
+    //             "User-Agent":
+    //                 "Instagram 85.0.0.21.100 Android (23/6.0.1; 538dpi; 1440x2560; LGE; LG-E425f; vee3e; en_US)",
+    //         },
+    //     }).then(response => response.json());
 
-        return response;
-    }
+    //     return response;
+    // }
 
     // async get_instagram_profile(username) {
     //     const userInfoSource = await fetch(`https://www.instagram.com/${username}/`, {
